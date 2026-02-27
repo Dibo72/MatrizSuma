2b4ec76a243c20b24b20efc42b145a1081f9c93e
Dibo72 <humanida133@gmail.com>
2026-02-27 09:18:02 +0100

    Ejercicio acabado

--- src/MatrizSuma.java
+++ src/MatrizSuma.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class MatrizSuma {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[][] matriz = new int[3][3];
+        int n = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Introduce el valor numero " + (n));
+                matriz[i][j] = sc.nextInt();
+                n++;
+            }
         }
+
+        System.out.println("Matriz:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Suma de filas:");
+        int sumaFilas = 0;
+        n = 1;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                sumaFilas += matriz[i][j];
+            }
+            System.out.println("Suma de la fila numero " + n + ": " + sumaFilas);
+            n++;
+            sumaFilas = 0;
+        }
+        System.out.println();
+
+        System.out.println("Suma de la columnas:");
+        int sumaColumnas = 0;
+        n = 1;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                sumaColumnas += matriz[j][i];
+            }
+            System.out.println("Suma de la columna numero " + n + ": " + sumaColumnas);
+            n++;
+            sumaColumnas = 0;
+        }
+        System.out.println();
+
+        System.out.println("Suma de la diagonal principal:");
+        int sumaDiagonal = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            sumaDiagonal += matriz[i][i];
+        }
+        System.out.println(sumaDiagonal);
+
     }
 }
